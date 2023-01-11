926704089e63b064f009c47a4cba717097bbf093
Fernando Fernandes <fernando.fernandes.opah.com.br>
2023-01-11 08:43:10 -0300

    subindo a aplicação

---
+++ README.md
@@ -0,0 +1,5 @@
+rode docker-compose up
+
+ou  docker build -t <nomeDaImagem> .
+
+docker run -p 3000:3000 <nomeDaImagem>
---
+++ src/data.js
@@ -0,0 +1,22 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'Carla Dias',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text:
+      "Estagiário",
+  },
+  {
+    id: 2,
+    name: 'Ana Queiroz',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Sênior.',
+  },
+];
+
+export default reviews;
